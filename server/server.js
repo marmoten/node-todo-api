6686974c94fef5e9d543c8a84a7b2c5c5e62b3ce
David Wiggins <david@kartograph.net>
2016-11-27 15:59:28 -0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
+// outside dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
+//local dependencies
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -31,8 +34,35 @@ app.get('/todos', (req, res) => {
 	});
 });
 
+// Get /todos/1234324
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		} else {
+			return res.send({todo});
+		}
+		console.log('Todo By ID', todo);
+	}, (e) => {
+		res.status(400).send();
+	});
+});
+
+
 app.listen(3000, () => {
 	console.log('Started on port 3000');
 });
 
 module.exports = {app};
+
+
+
+
+
+
