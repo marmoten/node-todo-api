d6feeb3f2d7d2cd989865adc4cea52ce91fae0b3
David Wiggins <david@kartograph.net>
2016-11-27 16:19:15 -0800

    Add test cases for GET /todos:id

--- server/server.js
+++ server/server.js
@@ -45,13 +45,12 @@ app.get('/todos/:id', (req, res) => {
 	Todo.findById(id).then((todo) => {
 		if (!todo) {
 			return res.status(404).send();
-		} else {
-			return res.send({todo});
 		}
-		console.log('Todo By ID', todo);
-	}, (e) => {
+		
+		res.send({todo});
+	}).catch((e) => {
 		res.status(400).send();
-	});
+	})
 });
 
 
